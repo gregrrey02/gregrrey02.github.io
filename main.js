695c0d25d0a929b7f14eb7157682573dd648ee74
gregrrey02 <jan.r.reyes@torontomu.ca>
2024-10-30 21:19:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-function nameButton(){
-   var your_name = prompt("Please enter your name:");
-   var text
-   if (your_name == null || your_name == ""){
-    text = "User cancelled the prompt";}
-    else {
-        text = "Hello " + your_name + ", welcome!";
-    }
-   document.getElementById("b_output").innerHTML = text; 
-}
 
 function imgButton(){
     var img = document.getElementById("img1");
@@ -18,4 +8,12 @@ function reset(){
     var img = document.getElementById("img1");
     img.style.transform = "scale(1)";
     img.style.transition = "transform 0.2s ease";
+}
+
+var submitButton = document.getElementById('submit')
+submitButton.onclick = saveData;
+
+function saveData(){
+  var input = document.getElementById("myform");
+  localStorage.setItem("myform", input.value);
 }
